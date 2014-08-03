73359e7be1dc5e4b8c5804065b0ef44155fa6561
Zeroh729 <joyceplayingcp@yahoo.com>
2014-08-04 06:07:42 +0800

    Working Splash Screen

--- src/com/ube/salinlahifour/MainActivity.java
+++ src/com/ube/salinlahifour/MainActivity.java
@@ -3,32 +3,28 @@ package com.ube.salinlahifour;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Handler;
 
 public class MainActivity extends Activity {
 
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
+		int SPLASH_TIME = 2 * 1000;// 3 seconds
 		final Intent intent = new Intent(this, SelectUserActivity.class);
 		
-		Thread timer = new Thread(){
-			public void run(){
-			try{
-				sleep(5000);
-			}catch(InterruptedException e){
-				e.printStackTrace();
-			}finally{
-				Log.d("Starting Activity","SelectUserActivty");
-				startActivity(intent);
-			}
-			}
-		};
-		
-		timer.run();
+		   new Handler().postDelayed(new Runnable() {
+
+		        public void run() {
+		           startActivity(intent);
+		           MainActivity.this.finish();
+		        }    
+
+		    }, SPLASH_TIME);
 
 	}
 	
