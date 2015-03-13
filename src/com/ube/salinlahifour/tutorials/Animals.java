a59f4553f768719e9b8854bbb57107f0034bb352
Zeroh729 <joyceplayingcp@yahoo.com>
2015-03-13 17:51:26 +0800

    Game 5 playable and beautiful

---
+++ src/com/ube/salinlahifour/tutorials/Animals.java
@@ -0,0 +1,33 @@
+package com.ube.salinlahifour.tutorials;
+
+import com.ube.salinlahifour.R;
+
+public class Animals extends AbstractTutorialActivity {
+
+	public Animals(){
+		layoutID = R.layout.tutorial_animals;
+	}
+	
+	@Override
+	protected void setEasyTutorial() {
+		setContentView(R.layout.tutorial_animals);
+	}
+
+	@Override
+	protected void setMediumTutorial() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void setHardTutorial() {
+		// TODO Auto-generated method stub
+
+	}
+	@Override
+	protected void initiateViews() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
