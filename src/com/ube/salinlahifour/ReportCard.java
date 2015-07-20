aa5af79300e0acfe28066924bf044851fec1963c
Skobeloff <raizadeleon_08@yahoo.com>
2015-07-20 22:06:15 +0800

    proceed to next lesson

--- src/com/ube/salinlahifour/ReportCard.java
+++ src/com/ube/salinlahifour/ReportCard.java
@@ -19,6 +19,7 @@ import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
 import com.ube.salinlahifour.animation.AnimatedButtonListener;
 import com.ube.salinlahifour.enumTypes.LevelType;
+import com.ube.salinlahifour.enumTypes.StarType;
 import com.ube.salinlahifour.evaluationModule.Evaluation;
 import com.ube.salinlahifour.tutorials.Tutorial;
 
@@ -94,7 +95,7 @@ public class ReportCard extends PopupWindow implements OnClickListener {
 		Log.d("UGH", "ItmSize:" + evaluation.getTotalScore());
 		tv_evaluation.setText("Hello");
 		Log.d("EndFeedback Debug", "Feedback: " + evaluation.getEndofActivityFeedback(evaluation.getScore(), lesson.getLessonNumber()));
-		tv_evaluation.setText(evaluation.getEndofActivityFeedback(evaluation.getScore(), lesson.getLessonNumber()));
+		//tv_evaluation.setText(evaluation.getEndofActivityFeedback(evaluation.getScore(), lesson.getLessonNumber()));
 
 		switch (evaluation.getStar()) {
 			case GOLD:
@@ -132,6 +133,13 @@ public class ReportCard extends PopupWindow implements OnClickListener {
 			btn_next.setVisibility(View.INVISIBLE);
 		} else {
 			Log.d("Debug ReportCard", "playNext Missing");
+		}
+		
+		if((evaluation.getStar().equals(StarType.GOLD) || evaluation.getStar().equals(StarType.SILVER))
+				&& level.equals(LevelType.HARD)) {
+			tv_evaluation.setText(evaluation.getEndofActivityFeedback(evaluation.getScore(), lesson.getLessonNumber()) + "Congratulations! You can now proceed to the next lesson!");
+		} else {
+			tv_evaluation.setText(evaluation.getEndofActivityFeedback(evaluation.getScore(), lesson.getLessonNumber()));
 		}
 	}
 
