7c732e891b0bdb3b46bc3a7d07fade886a1066de
sixZila <miguelcollantes1@yahoo.com>
2015-07-24 17:56:48 +0800

    shuffled choices in sentence construction

--- src/com/ube/salinlahifour/lessonActivities/Construction.java
+++ src/com/ube/salinlahifour/lessonActivities/Construction.java
@@ -1,21 +1,18 @@
 package com.ube.salinlahifour.lessonActivities;
 
 import java.util.ArrayList;
-
+import java.util.Collections;
 import android.graphics.Color;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import com.ube.salinlahifour.Item;
 import com.ube.salinlahifour.R;
 import com.ube.salinlahifour.SalinlahiFour;
-import com.ube.salinlahifour.enumTypes.LevelType;
 
 public class Construction extends AbstractLessonActivity implements OnClickListener {
 
@@ -26,10 +23,10 @@ public class Construction extends AbstractLessonActivity implements OnClickListe
 	private ArrayList<String> splittedWords;
 	private Button nextButton;
 	private Button checkAnswerButton;
-	private ArrayList<Item> shuffledChoices;
 	private String givenSentence;
 	private Button testButton;
 	private LinearLayout lin;
+
 	public Construction() {
 		layoutID = R.layout.lessonactivity_cons;
 	}
@@ -60,18 +57,19 @@ public class Construction extends AbstractLessonActivity implements OnClickListe
 		choiceBtnArray[1] = (Button) findViewById(R.id.choice_2);
 		choiceBtnArray[2] = (Button) findViewById(R.id.choice_3);
 		choiceBtnArray[3] = (Button) findViewById(R.id.choice_4);
-		
+
 		answerBtnArray = new Button[3];
 		answerBtnArray[0] = (Button) findViewById(R.id.answer_1);
 		answerBtnArray[1] = (Button) findViewById(R.id.answer_2);
 		answerBtnArray[2] = (Button) findViewById(R.id.answer_3);
 		splittedWords = new ArrayList<String>();
-		//testAddButtonProgramatically();
-		
+		// testAddButtonProgramatically();
+
 	}
-	//TEST FUNCTION DO NOT USE
-	private void testAddButtonProgramatically(){
-		
+
+	// TEST FUNCTION DO NOT USE
+	private void testAddButtonProgramatically() {
+
 		lin = (LinearLayout) findViewById(R.id.layout_answer);
 		RelativeLayout.LayoutParams p = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
 		p.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
@@ -86,18 +84,19 @@ public class Construction extends AbstractLessonActivity implements OnClickListe
 		testButton.setOnClickListener(this);
 		lin.addView(testButton);
 	}
+
 	@Override
 	protected void update() {
 		nextButton.setVisibility(View.INVISIBLE);
 		checkAnswerButton.setVisibility(View.VISIBLE);
 		answerBtnArray[2].setVisibility(View.GONE);
 		clearAnswers();
-		for(int x = 0; x < choiceBtnArray.length; x++){
+		for(int x = 0; x < choiceBtnArray.length; x++) {
 			choiceBtnArray[x].setVisibility(View.GONE);
 		}
 		if(getQuestionItem().getWord().split(" ").length >= 3)
 			answerBtnArray[2].setVisibility(View.VISIBLE);
-		for(int x = 0; x<getQuestionItem().getWord().split(" ").length;x++){
+		for(int x = 0; x < getQuestionItem().getWord().split(" ").length; x++) {
 			choiceBtnArray[x].setVisibility(View.VISIBLE);
 		}
 		loadChoiceButtons();
@@ -106,23 +105,23 @@ public class Construction extends AbstractLessonActivity implements OnClickListe
 		tv_dialog.setText(givenSentence);
 	}
 
-	private void loadChoiceButtons(){
-		ArrayList<Item> items = new ArrayList();
-		items.add(getQuestionItem());
-		items.add(loadShuffledChoices(getCntQuestions()).get(1));
+	private void loadChoiceButtons() {
 		splittedWords.clear();
-		for(Item item : items){
-			String[] words = item.getWord().split(" ");
-			for(int i = 0; i < words.length; i++){
-				if(splittedWords.size() < choiceBtnArray.length){
-					String temp = words[i].replace("_", " ");
-					splittedWords.add(temp);
-				}else{
-					break;
+		
+		String[] words = getQuestionItem().getWord().split(" ");
+		
+		for(int i = 0; i < words.length; i++) {
+			if(splittedWords.size() < choiceBtnArray.length) {
+				String temp = words[i].replace("_", " ");
+				splittedWords.add(temp);
+			} else {
+				break;
 			}
 		}
-		}
-		for(int i = 0; i < choiceBtnArray.length; i++){
+		
+		Collections.shuffle(splittedWords);
+
+		for(int i = 0; i < splittedWords.size(); i++) {
 			choiceBtnArray[i].setText(splittedWords.get(i));
 		}
 	}
@@ -130,7 +129,7 @@ public class Construction extends AbstractLessonActivity implements OnClickListe
 	@Override
 	protected void ifAnswerIsCorrect() {
 		tv_dialog.setText(feedback);
-		
+
 		checkAnswerButton.setVisibility(View.INVISIBLE);
 		nextButton.setVisibility(View.VISIBLE);
 	}
@@ -144,12 +143,14 @@ public class Construction extends AbstractLessonActivity implements OnClickListe
 	public void onClick(View v) {
 		int btnIndex = -1;
 		int ansIndex = -1;
-		//Log.d("Kaartehan", "View ID: "+ v.getId());
-		//if(testButton.getId()== v.getId()){
-		//	Log.d("Kaartehan", "If's worked!");
-		//}
+		// Log.d("Kaartehan", "View ID: "+ v.getId());
+		// if(testButton.getId()== v.getId()){
+		// Log.d("Kaartehan", "If's worked!");
+		// }
 		switch (v.getId()) {
-			case 1: Log.d("Kaartehan", "It worked!");break;
+			case 1:
+				Log.d("Kaartehan", "It worked!");
+				break;
 			case R.id.choice_1:
 				btnIndex = 0;
 				break;
@@ -180,26 +181,26 @@ public class Construction extends AbstractLessonActivity implements OnClickListe
 				break;
 		}
 
-		if(btnIndex != -1){
+		if(btnIndex != -1) {
 			addToAnswers(choiceBtnArray[btnIndex].getText().toString());
 		}
 
-		if(ansIndex != -1){
+		if(ansIndex != -1) {
 			answerBtnArray[ansIndex].setText("");
 		}
 	}
 
-	private void clearAnswers(){
-		for(Button butt : answerBtnArray){
+	private void clearAnswers() {
+		for(Button butt : answerBtnArray) {
 			butt.setText("");
 		}
 	}
 
-	private String formatAnswer(){
+	private String formatAnswer() {
 		String answer = "";
-		for(int i = 0; i < answerBtnArray.length; i++){
-			if(answerBtnArray[i].getVisibility() == View.VISIBLE){
-				if(i != 0){
+		for(int i = 0; i < answerBtnArray.length; i++) {
+			if(answerBtnArray[i].getVisibility() == View.VISIBLE) {
+				if(i != 0) {
 					answer += " ";
 				}
 				String temp = ((String) answerBtnArray[i].getText()).replace(" ", "_");
@@ -209,10 +210,10 @@ public class Construction extends AbstractLessonActivity implements OnClickListe
 		return answer;
 	}
 
-	private void addToAnswers(String answer){
-		for(int i = 0; i < answerBtnArray.length; i++){
-			if(answerBtnArray[i].getVisibility() == View.VISIBLE){
-				if(answerBtnArray[i].getText().equals("")){
+	private void addToAnswers(String answer) {
+		for(int i = 0; i < answerBtnArray.length; i++) {
+			if(answerBtnArray[i].getVisibility() == View.VISIBLE) {
+				if(answerBtnArray[i].getText().equals("")) {
 					answerBtnArray[i].setText(answer);
 					break;
 				}
